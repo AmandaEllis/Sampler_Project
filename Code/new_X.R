484b0c2dbc890650dee40f5890b54db4adbdb7d9
AmandaEllis <arelli4@uky.edu>
2016-04-12 11:55:22 -0400

    Changed candidate X to only move single photo at a time.  Also added in density of the proposal distribution.

--- Code/new_X.R
+++ Code/new_X.R
@@ -1,42 +1,40 @@
 #Function that generates a new X
-#New X is chosen by first selecting k photos from the first capture occasion.  
+#New X is chosen by selecting a single photograph from a random ocassion 
 #These photos are then randomly assigned to a different individual
 #This is repeated for each capture occasion
 
-new.X<-function(X,k,t){
+new.X<-function(X,t){
 prev.ind<-max(X[,3])  #Computes the number of individuals in current X
 new.X<-X
-#For each capture occasion choose k photos and randomly assign to a different individual
-for(j in 1:t){
-    #choose removal location for k photos and remove those photos
-    remove.location<-sample(X[which(X[,2]==j),1],k)
-    remove.photos<-X[remove.location,1]
-    
-    #Relocate the k photos
-    new.temp<-1
-    for(l in 1:k){
-    #sample individual and place photo 
-    #Individual is sampled from current individuals plus allowance for new individual
-    new.individual<-sample(1:(prev.ind+new.temp),1)
-    new.X[remove.location[l],3]<-new.individual
-      if(new.individual==prev.ind+new.temp){new.temp=new.temp+1}
-    } 
-  }
+
+#choose removal location for photo and set individual for photo equal to NA
+remove.location<-sample(X[which(X[,2]==t),1],1)
+new.X[remove.location,3]<-NA
 
 #If we remove all of the photographs for an individual we need to renumber the individuals
 #Otherwise we will have zero rows in W and favor moving photographs to a new individuals 
 #when selecting new X.
-  new.ID<-sort(unique(new.X[,3]))  #Current IDs in new X
-  old.ID<-c(1:max(new.ID))          #IDs in previous X
-  lost.ID<-old.ID[-which(old.ID%in% new.ID)]  #IDs lost when generating new X
-  num.lost.ID<-length(lost.ID)
+new.ID<-sort(unique(new.X[,3]))  #Current IDs in new X
+old.ID<-c(1:prev.ind)            #IDs in previous X
+lost.ID<-old.ID[-which(old.ID%in% new.ID)]  #IDs lost when generating new X
+num.lost.ID<-length(lost.ID)  #length is 1 or zero.
 
-  if(num.lost.ID>0){
-  lost.ID<-c(1,lost.ID,(max(new.ID)+1))
-  for(j in 1:(num.lost.ID+1)){
-    new.X[which(new.X[,3]>=lost.ID[j]&new.X[,3]<lost.ID[j+1]),3]<-new.X[which(new.X[,3]>=lost.ID[j]&new.X[,3]<lost.ID[j+1]),3]-(j-1)
-    }
-  }
-  return(new.X)
+#if an ID was lost we renumber the individuals    
+if(num.lost.ID>0){
+      temp<-c(1,lost.ID,(max(new.ID)+1))
+      for(j in 1:2){
+      new.X[which(new.X[,3]>=temp[j]&new.X[,3]<temp[j+1]),3]<-new.X[which(new.X[,3]>=temp[j]&new.X[,3]<temp[j+1]),3]-(j-1)
+      }
+}
+
+new.ind<-max(X[,3]) #Computes the number of individuals in new X
+
+#Relocate the photo
+#sample individual and place photo 
+#Individual is sampled from current individuals plus allowance for new individual
+new.individual<-sample(1:(new.ind+1),1)
+new.X[remove.location,3]<-new.individual
+
+return(list("new.X"=new.X,"n.ID.cand.X"=new.ind,"n.ID.curr.X"=prev.ind))
 
 }
